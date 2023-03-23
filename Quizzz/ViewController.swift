4f95fc1e40a855d33bdcca6f29ea2d591b541a40
MksMil <maksymmiliaiev@gmail.com>
2023-03-23 08:45:08 +0200

    Initial Commit

---
+++ Quizzz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzz
+//
+//  Created by Миляев Максим on 23.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
