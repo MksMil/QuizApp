980772d8ba1c6e25e2b4513a0c9c662b3bdf7bcb
MksMil <maksymmiliaiev@gmail.com>
2023-04-05 06:59:43 +0300

    generate quizapp workspace. add quiz engine framework to workspace. add new target - quizEngine_iOS, and add this target to files targets. change product name in build settings

--- QuizEngine/QuizEngine/Game.swift
+++ QuizEngine/QuizEngine/Game.swift
@@ -7,19 +7,17 @@ public class Game<Question: Hashable,Answer,R:Router> where R.Question == Questi
         self.flow = flow
     }
 }
-    
-public func startGame<Question:Hashable, Answer: Equatable, R:Router>(questions: [Question],router: R, correctAnswers:[Question: Answer])->Game<Question,Answer,R> where R.Question == Question, R.Answer == Answer{
+
+public func startGame<Question:Hashable, Answer: Equatable, R:Router>(questions: [Question],router: R, correctAnswers:[Question: Answer]) -> Game<Question,Answer,R> where R.Question == Question, R.Answer == Answer{
     let flow = Flow(questions: questions, router: router, scoring:{ scoring(answers:$0, correctAnswers: correctAnswers)})
     flow.start()
     return Game(flow: flow)
 }
 
 private func scoring<Question: Hashable, Answer: Equatable>(answers: [Question: Answer], correctAnswers: [Question: Answer]) -> Int{
-    
-    
-    return answers.reduce(0) { (scores, elementTuple) in
-        return scores + (correctAnswers[elementTuple.key] == elementTuple.value ? 1 : 0)
+ return answers.reduce(0) { (scores, tuple) in
+        return scores + (correctAnswers[tuple.key] == tuple.value ? 1 : 0)
     }
 }
-    
+
 
