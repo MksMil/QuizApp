bc8e944f7455f16614a9c9ad03921c585ff2508c
MksMil <maksymmiliaiev@gmail.com>
2023-04-26 17:39:06 +0300

    Made final in SceneDelegate and finished first version of app

--- QuizApp/QuizApp/SceneDelegate.swift
+++ QuizApp/QuizApp/SceneDelegate.swift
@@ -1,36 +1,46 @@
-//
-//  SceneDelegate.swift
-//  QuizApp
-//
-//  Created by Миляев Максим on 24.03.2023.
-//
-
 import UIKit
+import QuizEngine
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
+    var game: Game<Question<String>,[String],NavigationControllerRouter>?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let scene = (scene as? UIWindowScene) else { return }
+        
+        let question1 = Question.singleSelection("Сколько стоит мясо?")
+        let question2 = Question.multipleSelection("Где живут рыбы?")
+        let question3 = Question.singleSelection("Как называется наша родная планета?")
+        let question4 = Question.multipleSelection("Что нужно есть на завтрак?")
+        
+        let questions = [question1,question2,question3,question4]
+        
+        let options = [question1: ["100", "200", "300"],
+                       question2: ["Океан", "Аквариум", "Горы","Лес"],
+                       question3: ["Нептун", "Марс", "Сатурн","Земля","Юпитер"],
+                       question4: ["Гвозди", "Яичницу", "Тосты","Салат","Пюрешку с котлеткой"]
+        ]
+        let correctAnswers = [question1: ["200"],
+                              question2: ["Океан", "Аквариум"],
+                              question3: ["Земля"],
+                              question4: ["Яичницу", "Тосты","Салат"]]
+        
+        let navController = UINavigationController()
+        let factory = iOSViewControllerFactory(questions: questions,
+                                               options: options,
+                                               correctAnswers: correctAnswers)
+        let router = NavigationControllerRouter(navController,
+                                                factory: factory)
         window = UIWindow(windowScene: scene)
-//        let vc = QuestionViewController(question: "Question",
-//                                        options: ["Answer1", "Answer2", "Answer3"])
-//        { print($0) }
-//        _ = vc.view
-//        vc.tableView.allowsMultipleSelection = true
-   
-        let vc = ResultViewController(summary: "Your results is : 1/2", answers: [
-        PresentableAnswer(question: "First QuestionFirst QuestionFirst QuestionFirst QuestionFirst QuestionFirst QuestionFirst QuestionFirst QuestionFirst QuestionFirst QuestionFirst Question", answer: "Right AnswerRight AnswerRight AnswerRight AnswerRight AnswerRight AnswerRight AnswerRight AnswerRight Answer", wrongAnswer: nil),
-        PresentableAnswer(question: "FSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond Question", answer: "RiFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond Questionght Answer", wrongAnswer: "WrongFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond QuestionFSecond Question Answer"),
-        PresentableAnswer(question: "Third Question", answer: "Right Answer", wrongAnswer: "NOOO!"),
-        PresentableAnswer(question: "Fourth Question", answer: "YES!!!!", wrongAnswer: nil)
-        ])
-        let nav = UINavigationController(rootViewController: vc)
-        window?.rootViewController = nav
+        
+        window?.rootViewController = navController
         
         window?.makeKeyAndVisible()
+        
+        game = startGame(questions: questions,
+                             router: router,
+                             correctAnswers: correctAnswers)
     }
 }
 
